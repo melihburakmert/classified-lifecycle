7f3b3e06152c7d7cde5a4264ea8e6146108ae12b
melihburakmert <melih.burak.mert@gmail.com>
2025-08-21 17:35:17 +0200

    Use virtual threads

--- src/main/java/mbm/classified_lifecycle/common/config/AsyncConfig.java
+++ src/main/java/mbm/classified_lifecycle/common/config/AsyncConfig.java
@@ -1,28 +1,20 @@
 package mbm.classified_lifecycle.common.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
-@RequiredArgsConstructor
 public class AsyncConfig {
-
-  private final ThreadPoolProperties threadPoolProperties;
 
   @Bean(name = "expirationTaskExecutor")
   public TaskExecutor taskExecutor() {
-
-    final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(threadPoolProperties.corePoolSize());
-    executor.setMaxPoolSize(threadPoolProperties.maxPoolSize());
-    executor.setQueueCapacity(threadPoolProperties.queueCapacity());
-    executor.setThreadNamePrefix(threadPoolProperties.threadNamePrefix());
-    executor.initialize();
-    return executor;
+    return new TaskExecutorAdapter(
+        Executors.newVirtualThreadPerTaskExecutor());
   }
 }
